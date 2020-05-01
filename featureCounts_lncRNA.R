5ae3675c9567666c48421557a2475c27c697a7e8
josrpowe208 <60009904+josrpowe208@users.noreply.github.com>
2020-05-01 16:48:13 -0400

    Update featureCounts_lncRNA.R

--- featureCounts_lncRNA.R
+++ featureCounts_lncRNA.R
@@ -1,15 +1,17 @@
+# Load libraries, set up working directory
 suppressPackageStartupMessages(library(Rsubread))
 
 setwd("/mnt/pan/courses/sybb412/jrp208/cocaine_project")
 
+# Create list for featureCounts output
 lst_esetParts <- list()
 
+# Read in sample table data
 sampleTable <- read.delim(file = "/mnt/pan/courses/sybb412/jrp208/PRJNA388174_cocaine/SraRunTable-2.txt", header = TRUE, row.names = 1)
 
 filenames <- list.files(path = '.', pattern = "hisat[[:punct:]]bam$")
 
 # Read in .bam files
-
 fc <- featureCounts(files=filenames, annot.ext="/mnt/pan/courses/sybb412/jrp208/genomes/gencode.v19.long_noncoding_RNAs.gtf", isGTFAnnotationFile=TRUE, isPairedEnd=TRUE)
 
 colnames(fc$counts) <- sampleTable$Run
@@ -18,13 +20,11 @@ lst_esetParts$EsetRaw <- fc
 lst_esetParts$sampleTable <- sampleTable
 
 
-# Create the parameter values
-
+# Save RDS data
 currentTime <- paste(format(Sys.time(), "%Y-%m-%dt%H_%M_%S"))
 
 savePath <- file.path(path = '.',paste('save', currentTime, "RDS", sep="."))
 
-# Save the analysis
 saveRDS(lst_esetParts, file = savePath )
 lastSaved <- currentTime
 
@@ -34,4 +34,4 @@ rm(currentTime)
 sessionInfo()
 
 
-#End of create InData object.
+#End of create featureCounts object.
